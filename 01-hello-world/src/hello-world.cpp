60bf6c2db56b7b3a72d3279896270e0acc800ecf
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2022-08-28 03:29:52 +0430

    Add basic structure for the first example

---
+++ 01-hello-world/src/hello-world.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file hello-world.cpp
+ * @author Hedayat Vatankhah (hedayat.fwd@gmail.com)
+ * @brief Hello World!
+ * @version 0.1
+ * @date 2022-08-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is NOT the intended \"Hello World\"!\n";
+
+    return 0;
+}
