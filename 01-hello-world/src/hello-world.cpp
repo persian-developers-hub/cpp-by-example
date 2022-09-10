1d285048d33591131343c450432d3de61094e1c4
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2022-09-11 01:16:15 +0430

    Add a cout example & honor max line length at 80 chars

--- 01-hello-world/src/hello-world.cpp
+++ 01-hello-world/src/hello-world.cpp
@@ -4,14 +4,15 @@
  * @brief Hello World!
  * @version 0.1
  * @date 2022-08-28
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 /*
- * #include is a preprocessor directive. Effectively, the contents of the speicified file (which is found
- * in compiler's 'include path') is pasted here completely.
+ * #include is a preprocessor directive. Effectively, the contents of the
+ * speicified file (which is found in compiler's 'include path') is pasted here
+ * completely.
  */
 
  // iostream is a standard header file providing some I/O facilities
@@ -20,20 +21,21 @@
 // httplib is a thirdparty library and httplib.h is its main header
 #include <httplib.h>
 
-// Boost is a well-known third-party general purpose library consisting of many smaller
-// libraries. Boost Json is a relatively new addition to the boost collection, however
-// it is still not present in some GNU/Linux distros; e.g. Ubuntu.
-// So, we use our own copy of it for now.
+// Boost is a well-known third-party general purpose library consisting of many
+// smaller libraries. Boost Json is a relatively new addition to the boost
+// collection, however it is still not present in some GNU/Linux distros;
+// e.g. Ubuntu. So, we use our own copy of it for now.
 #include <boost/json/serialize.hpp>
 #include <boost/json/value.hpp>
 
-// src.hpp is only needed in a single file if we are not linking to boost json library
+// src.hpp is only needed in a single file if we are not linking to boost json
+// library
 #include <boost/json/src.hpp>
 
 /*
- * 'using namespace' brings all 'names' in a 'namespace' into the current namespace,
- * so that we can use their names without the namespace prefix, e.g. we can use
- * cout instead of std::cout.
+ * 'using namespace' brings all 'names' in a 'namespace' into the current
+ * namespace, so that we can use their names without the namespace prefix,
+ * e.g. we can use cout instead of std::cout.
  * Should be avoided in header files unless there is a good justification for it
  * which is almost never!
  */
@@ -43,14 +45,18 @@ using namespace std;
 namespace json = boost::json;
 
 /*
- * Normally, every C++ 'program' needs to have a 'main' function, which is called
- * at application startup by the OS.
+ * Normally, every C++ 'program' needs to have a 'main' function, which is
+ * called at application startup by the OS.
  */
 int main()
 {
-    // Define an object (variable) named 'server' of type 'httplib::Server', which is
-    // 'Server' class in 'httplib' namespace.
+    // Define an object (variable) named 'server' of type 'httplib::Server',
+    // which is 'Server' class in 'httplib' namespace.
     httplib::Server server;
+
+    /*
+     * Now, we call "member functions" (methods) of the "server" object.
+     */
 
     // simple text answer for '/hello' path
     server.Get("/hello",
@@ -60,9 +66,11 @@ int main()
     });
 
     // json answer for '/hello-json' path, also using passed param (if any)
-    server.Get("/hello-json", [](const httplib::Request &req, httplib::Response &resp) {
-        // 'auto' in variable definition with assignemnt: let the compiler automatically detect the
-        // type using the type of the assigned value
+    server.Get("/hello-json", [](const httplib::Request &req,
+                                    httplib::Response &resp) {
+
+        // 'auto' in variable definition with assignemnt: let the compiler
+        // automatically detect the type using the type of the assigned value
         auto name = req.get_param_value("name");
 
         // 'list initialization' of hw variable
@@ -73,6 +81,16 @@ int main()
         resp.set_content(json::serialize(hw), "application/json");
     });
 
+
+    /*
+     * 'cout' is the standard "character output" object provided by C++
+     * 'endl' is an 'io manipulator' to print end line and flush output buffer
+     * '<<' is actually C++ "left shift" operator; however it is "overloaded"
+     * as an "output operator" in iostream library
+     */
+    cout << "Starting the service on port 8080..." << endl;
+
+    // Run the web server (blocking)
     server.listen("0.0.0.0", 8080);
 
     // Return to OS with exit code '0', which means success
